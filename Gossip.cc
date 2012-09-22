fa871f560ee4d9ec4eb48040e09c1e64a2e5e2f7
Roshan Ghumare <ornash.g@gmail.com>
2012-09-22 17:13:13 -0400

    Using packetsSent so far for expected sequence.

--- Gossip.cc
+++ Gossip.cc
@@ -10,7 +10,7 @@ void Gossip::startup() {
 
 	lateResponse = droppedRequests = rounds = wait = expectedSeq = packetsSent = 0;
 	gSend = gReceive = gRespond = 0;
-	roundsBeforeStopping = par("stopGossipAfter") ;
+	roundsBeforeStopping = par("stopGossipAfter");
 	neighbourCheckInterval = STR_SIMTIME(par("neighbourCheckInterval"));
 	gossipInterval = STR_SIMTIME(par("gossipInterval"));
 	myInfo.id = self;
@@ -23,6 +23,7 @@ void Gossip::startup() {
 	//gossipMsg = par("gossipMsg");
 	isBusy = false;
 
+	//assignNeighbours(self);
 	// Start sharing neighbours after some initial interval.
 	out << nodeStartupDiff;
 	temp = out.str();
@@ -37,11 +38,13 @@ void Gossip::startup() {
 	temp2 += "ms";
 	setTimer(START_GOSSIP, STR_SIMTIME(temp2.c_str()));
 
-	temp2 = "5000ms";
-	setTimer(SAMPLE_AVG, STR_SIMTIME( temp2.c_str() ));
+/*
+	temp2 = "6000ms";
+	setTimer(SAMPLE_AVG, STR_SIMTIME(temp2.c_str()));
+*/
 
-	 declareOutput("Wasted Requests");
-	 declareOutput("Stats");
+	declareOutput("Wasted Requests");
+	declareOutput("Stats");
 }
 
 void Gossip::timerFiredCallback(int type) {
@@ -51,19 +54,19 @@ void Gossip::timerFiredCallback(int type) {
 	vector<int> delIndices;
 	vector<PEERINFO> keepPeers;
 	double sum = 0.0;
-	int i =0;
+	int i = 0;
 	bool peersModified = false;
 
 	switch (type) {
 
-
-
-	  case SAMPLE_AVG:{
-		  trace() << "Current Avg = " << gossipMsg;
-		  temp2 = "5000ms";
-		  setTimer(SAMPLE_AVG, STR_SIMTIME( temp2.c_str() ));
-		 break;
-	  }
+	case SAMPLE_AVG: {
+/*		//trace() << "Current Avg = " << gossipMsg;
+		temp2 = "6000ms";
+		shallGossip = ( ((double) rand() / (double) RAND_MAX) > 0.5 ? true : false);
+		trace() << "Toss coin " << shallGossip;
+		setTimer(SAMPLE_AVG, STR_SIMTIME(temp2.c_str()));*/
+		break;
+	}
 
 	case GET_NEIGHBOUR: {
 		//trace() << "Request neighbours.";
@@ -71,7 +74,7 @@ void Gossip::timerFiredCallback(int type) {
 		keepPeers.push_back(peers[0]);
 		for (it = peers.begin() + 1, i = 1; it != peers.end(); ++it, i++) {
 			(*it).staleness++;
-			if((*it).staleness <= 3)
+			if ((*it).staleness <= 3)
 				keepPeers.push_back((*it)); // Still good, keep it.
 			else
 				peersModified = true; //A peer got deleted.
@@ -88,7 +91,8 @@ void Gossip::timerFiredCallback(int type) {
 		peers.at(0).neighbourCount = peers.size() - 1;
 		//Skip the first element which is the node itself.
 		for (it = peers.begin() + 1; it != peers.end(); ++it) {
-			(*it).weight = (double) 1 / (double) (1 + ((*it).neighbourCount > peers.at(0).neighbourCount ? (*it).neighbourCount : peers.at(0).neighbourCount));
+			(*it).weight = (double) 1 / (double) (1 + ((*it).neighbourCount	> peers.at(0).neighbourCount ?
+					(*it).neighbourCount : peers.at(0).neighbourCount));
 			sum += (*it).weight;
 			//(self == 2 || self == 3 || self == 27) &&  trace() << "Id = " << (*it).id << ", weight = " << (*it).weight	<< ", count = " << (*it).neighbourCount;
 		}
@@ -96,22 +100,22 @@ void Gossip::timerFiredCallback(int type) {
 		peers.at(0).weight = (double) 1 - sum;
 		//(self == 2 || self == 3 || self == 27) && trace() << "Id = " << peers.at(0).id << ", weight = " << peers.at(0).weight << ", count = " << peers.at(0).neighbourCount;
 		for (it = peers.begin() + 1; it != peers.end(); ++it) {
-			(*it).weight = (* (it - 1) ).weight + (*it).weight;
-		//(self == 2 || self == 3 || self == 27) && trace() << "Id = " << (*it).id << ", weight = " << (*it).weight << ", count = " << (*it).neighbourCount;
+			(*it).weight = (*(it - 1)).weight + (*it).weight;
+			//(self == 2 || self == 3 || self == 27) && trace() << "Id = " << (*it).id << ", weight = " << (*it).weight << ", count = " << (*it).neighbourCount;
 		}
 		//(self == 2 || self == 3 || self == 27) && trace() << "Id = " << peers.at(0).id << ", weight = " << peers.at(0).weight << ", count = " << peers.at(0).neighbourCount;
 
 		/*if( ( !peersModified && newPeers.size() == 0 ) && ( neighbourCheckInterval < 120.0 )) {
-				neighbourCheckInterval = neighbourCheckInterval + STR_SIMTIME(par("neighbourCheckInterval"));
-				(self == 2 || self == 3 || self == 27) && trace() << "Increase neighbourCheckInterval to " << neighbourCheckInterval;
-		}else if( neighbourCheckInterval > 5 ) {
-				neighbourCheckInterval = neighbourCheckInterval - STR_SIMTIME(par("neighbourCheckInterval"));
-				(self == 2 || self == 3 || self == 27) && trace() << "Decrease neighbourCheckInterval to " << neighbourCheckInterval;
-		}else{
-			(self == 2 || self == 3 || self == 27) && trace() << "No change in neighbourCheckInterval";
-		}*/
+		 neighbourCheckInterval = neighbourCheckInterval + STR_SIMTIME(par("neighbourCheckInterval"));
+		 (self == 2 || self == 3 || self == 27) && trace() << "Increase neighbourCheckInterval to " << neighbourCheckInterval;
+		 }else if( neighbourCheckInterval > 5 ) {
+		 neighbourCheckInterval = neighbourCheckInterval - STR_SIMTIME(par("neighbourCheckInterval"));
+		 (self == 2 || self == 3 || self == 27) && trace() << "Decrease neighbourCheckInterval to " << neighbourCheckInterval;
+		 }else{
+		 (self == 2 || self == 3 || self == 27) && trace() << "No change in neighbourCheckInterval";
+		 }*/
 		newPeers.clear();
-		toNetworkLayer(createGossipDataPacket(PULL_NEIGHBOUR, packetsSent++), BROADCAST_NETWORK_ADDRESS);
+		toNetworkLayer(createGossipDataPacket(PULL_NEIGHBOUR, packetsSent++),		BROADCAST_NETWORK_ADDRESS);
 		setTimer(GET_NEIGHBOUR, neighbourCheckInterval);
 		break;
 	}
@@ -142,17 +146,16 @@ void Gossip::timerFiredCallback(int type) {
 		} else
 			wait++;
 
-		if( roundsBeforeStopping != 0 )
+		if (roundsBeforeStopping != 0)
 			setTimer(START_GOSSIP, gossipInterval);
 		else {
-//			trace() << "Stop gossip at " << self;
+			//			trace() << "Stop gossip at " << self;
 			isBusy = false;
 		}
 		break;
 	}
 	}
 }
-
 
 void Gossip::enQueue(GOSSIP_EXCH_MSG peer) {
 	waitQueue.push(peer);
@@ -169,10 +172,11 @@ void Gossip::deQueue() {
 	stringstream out;
 	string neighbour;
 
-	for(i = 0; i < size; i++){
-		msg = waitQueue.front(); waitQueue.pop();
+	for (i = 0; i < size; i++) {
+		msg = waitQueue.front();
+		waitQueue.pop();
 		waitTime = currentTime - msg.receivedAt;
-		if(waitTime < timeOut) {
+		if (waitTime < timeOut) {
 			gossipMsg = (gossipMsg + msg.data) / 2; //Update Avg
 			sendData.data = gossipMsg;
 			sendData.seq = self;
@@ -182,22 +186,14 @@ void Gossip::deQueue() {
 			gRespond++;
 			toNetworkLayer(createGossipDataPacket(GOSSIP_PUSH, sendData, packetsSent++), neighbour.c_str());
 			//(self == 2 || self == 3 || self == 27) && trace() << "Dequeued requests: New avg  " << sendData.data << ", is being sent to " << msg.initiator;
-		}else{
+		} else {
 			droppedRequests++;
 		}
 	}
 }
 
-void Gossip::handleSensorReading(SensorReadingMessage * reading) {
-
-}
-
-void Gossip::handleNeworkControlMessage(cMessage * msg) {
-
-}
-
 void Gossip::fromNetworkLayer(ApplicationPacket * genericPacket,
-	const char *source, double rssi, double lqi) {
+		const char *source, double rssi, double lqi) {
 	GossipPacket *rcvPacket = check_and_cast<GossipPacket*> (genericPacket);
 	double msgType = rcvPacket->getData();
 	GossipInfo receivedData = rcvPacket->getExtraData();
@@ -213,19 +209,20 @@ void Gossip::fromNetworkLayer(ApplicationPacket * genericPacket,
 
 	case PULL_NEIGHBOUR:
 		//PUSH selfIP: Temporarily receiver will figure out this from the source IP, add sourceIP to packet later.
-		toNetworkLayer(createGossipDataPacket(PUSH_NEIGHBOUR,(int) peers.size() - 1, packetsSent++), source);
+		toNetworkLayer(createGossipDataPacket(PUSH_NEIGHBOUR,
+				(int) peers.size() - 1, packetsSent++), source);
 		break;
 
 	case PUSH_NEIGHBOUR:
 		//PULL response received, update neighbour list if new peer is discovered.
-		for(i = 0; i < peers.size(); i++) {
-			if(peers.at(i).id == peer) {
+		for (i = 0; i < peers.size(); i++) {
+			if (peers.at(i).id == peer) {
 				found = true;
 				peers.at(i).staleness = 0;
 				peers.at(i).neighbourCount = rcvPacket->getNeighbourCount();
 			}
 		}
-		if(!found) {
+		if (!found) {
 			neighbour.id = peer;
 			neighbour.staleness = 0;
 			neighbour.neighbourCount = rcvPacket->getNeighbourCount();
@@ -243,13 +240,13 @@ void Gossip::fromNetworkLayer(ApplicationPacket * genericPacket,
 			enQueue(msg);
 			//(self == 2 || self == 3 || self == 27) && trace() << "Add to queue "	<< source;
 		} else {
-			//Restart stopped gossip if required.
+			//Restart stopped gossip if required, this will certainly affect any ongoing schedule.
 			//(self == 2 || self == 3 || self == 27) && trace() << "roundsBeforeStopping " << roundsBeforeStopping << " gossipMsg " << gossipMsg << " receivedData.data " << receivedData.data;
-			if( roundsBeforeStopping <= 0 && (gossipMsg != receivedData.data)) {
+			if (roundsBeforeStopping <= 0 && ( !compareDouble(gossipMsg, receivedData.data) )) {
 				roundsBeforeStopping = (int) par("stopGossipAfter");
 				setTimer(START_GOSSIP, gossipInterval);
 				isBusy = false;
-	//			trace() << "Restart gossip at " << self;
+				//			trace() << "Restart gossip at " << self;
 			}
 			//Calculate avg, and share.
 			extraData.seq = receivedData.seq;
@@ -265,7 +262,8 @@ void Gossip::fromNetworkLayer(ApplicationPacket * genericPacket,
 		if (receivedData.seq == expectedSeq) {
 			gReceive++;
 			//Update avg, send ACK
-			if( (gossipMsg - receivedData.data) != 0) {
+			//TO DO: Code the test for equality of doubles properly.
+			if ( !compareDouble(gossipMsg, receivedData.data) ) {
 				gossipMsg = receivedData.data;
 				//(self == 2 || self == 3 || self == 27) && trace() << "Update avg to " << gossipMsg << ", peer was " << source;
 			} else {
@@ -304,15 +302,13 @@ GossipPacket* Gossip::createGossipDataPacket(double data, unsigned int seqNum) {
 	return newPacket;
 }
 
-GossipPacket* Gossip::createGossipDataPacket(double data, GossipInfo extra,
-		unsigned int seqNum) {
+GossipPacket* Gossip::createGossipDataPacket(double data, GossipInfo extra,	unsigned int seqNum) {
 	GossipPacket *newPacket = createGossipDataPacket(data, seqNum);
 	newPacket->setExtraData(extra);
 	return newPacket;
 }
 
-GossipPacket* Gossip::createGossipDataPacket(double data, int neighbourCount,
-		unsigned int seqNum) {
+GossipPacket* Gossip::createGossipDataPacket(double data, int neighbourCount, unsigned int seqNum) {
 	GossipPacket *newPacket = createGossipDataPacket(data, seqNum);
 	newPacket->setNeighbourCount(neighbourCount);
 	return newPacket;
@@ -330,4 +326,309 @@ void Gossip::finishSpecific() {
 	collectOutput("Stats", "Sent", gSend);
 	collectOutput("Stats", "Got Back", gReceive);
 	collectOutput("Stats", "Responded to", gRespond);
+}
+
+bool Gossip::compareDouble(double num1, double num2){
+	return (fabs(num1 - num2) <= EPSILON);
+}
+
+void Gossip::handleSensorReading(SensorReadingMessage * reading) {
+
+}
+
+void Gossip::handleNeworkControlMessage(cMessage * msg) {
+
+}
+
+void Gossip::assignNeighbours (int id) {
+	PEERINFO neighbour;
+	switch (id) {
+
+	case 0:
+		neighbour.id = 1;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 3;
+		newPeers.push_back(neighbour);
+
+		neighbour.id = 2;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 3;
+		newPeers.push_back(neighbour);
+
+		neighbour.id = 3;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 3;
+		newPeers.push_back(neighbour);
+
+		neighbour.id = 4;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 3;
+		newPeers.push_back(neighbour);
+
+		break;
+	case 1:
+		neighbour.id = 0;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 4;
+		newPeers.push_back(neighbour);
+
+		neighbour.id = 6;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 2;
+		newPeers.push_back(neighbour);
+
+		neighbour.id = 7;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 3;
+		newPeers.push_back(neighbour);
+		break;
+
+	case 2:
+		neighbour.id = 0;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 4;
+		newPeers.push_back(neighbour);
+
+		neighbour.id = 8;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 2;
+		newPeers.push_back(neighbour);
+
+		neighbour.id = 7;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 3;
+		newPeers.push_back(neighbour);
+		break;
+
+	case 3:
+		neighbour.id = 0;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 4;
+		newPeers.push_back(neighbour);
+
+		neighbour.id = 6;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 2;
+		newPeers.push_back(neighbour);
+
+		neighbour.id = 5;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 2;
+		newPeers.push_back(neighbour);
+		break;
+
+	case 4:
+		neighbour.id = 0;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 4;
+		newPeers.push_back(neighbour);
+
+		neighbour.id = 8;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 2;
+		newPeers.push_back(neighbour);
+
+		neighbour.id = 5;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 2;
+		newPeers.push_back(neighbour);
+		break;
+
+	case 5:
+		neighbour.id = 3;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 3;
+		newPeers.push_back(neighbour);
+
+		neighbour.id = 4;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 3;
+		newPeers.push_back(neighbour);
+		break;
+
+	case 6:
+		neighbour.id = 3;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 3;
+		newPeers.push_back(neighbour);
+
+		neighbour.id = 1;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 3;
+		newPeers.push_back(neighbour);
+		break;
+
+	case 8:
+		neighbour.id = 4;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 3;
+		newPeers.push_back(neighbour);
+
+		neighbour.id = 2;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 3;
+		newPeers.push_back(neighbour);
+		break;
+
+	case 7:
+		neighbour.id = 1;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 3;
+		newPeers.push_back(neighbour);
+
+		neighbour.id = 2;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 3;
+		newPeers.push_back(neighbour);
+
+		neighbour.id = 9;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 3;
+		newPeers.push_back(neighbour);
+		break;
+
+	case 9:
+		neighbour.id = 7;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 3;
+		newPeers.push_back(neighbour);
+
+		neighbour.id = 10;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 3;
+		newPeers.push_back(neighbour);
+
+		neighbour.id = 12;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 3;
+		newPeers.push_back(neighbour);
+		break;
+
+	case 13:
+		neighbour.id = 10;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 3;
+		newPeers.push_back(neighbour);
+
+
+		neighbour.id = 12;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 3;
+		newPeers.push_back(neighbour);
+
+		neighbour.id = 14;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 3;
+		newPeers.push_back(neighbour);
+
+		neighbour.id = 16;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 3;
+		newPeers.push_back(neighbour);
+		break;
+
+	case 16:
+		neighbour.id = 13;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 4;
+		newPeers.push_back(neighbour);
+
+		neighbour.id = 15;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 2;
+		newPeers.push_back(neighbour);
+
+		neighbour.id = 17;
+		neighbour.staleness = 0;
+		neighbour.neighbourCount = 2;
+		newPeers.push_back(neighbour);
+		break;
+
+	case 14:
+			neighbour.id = 13;
+			neighbour.staleness = 0;
+			neighbour.neighbourCount = 4;
+			newPeers.push_back(neighbour);
+
+			neighbour.id = 11;
+			neighbour.staleness = 0;
+			neighbour.neighbourCount = 2;
+			newPeers.push_back(neighbour);
+
+			neighbour.id = 17;
+			neighbour.staleness = 0;
+			neighbour.neighbourCount = 2;
+			newPeers.push_back(neighbour);
+			break;
+
+	case 12:
+			neighbour.id = 13;
+			neighbour.staleness = 0;
+			neighbour.neighbourCount = 4;
+			newPeers.push_back(neighbour);
+
+			neighbour.id = 9;
+			neighbour.staleness = 0;
+			neighbour.neighbourCount = 3;
+			newPeers.push_back(neighbour);
+
+			neighbour.id = 15;
+			neighbour.staleness = 0;
+			neighbour.neighbourCount = 2;
+			newPeers.push_back(neighbour);
+			break;
+
+	case 10:
+			neighbour.id = 0;
+			neighbour.staleness = 0;
+			neighbour.neighbourCount = 4;
+			newPeers.push_back(neighbour);
+
+			neighbour.id = 9;
+			neighbour.staleness = 0;
+			neighbour.neighbourCount = 3;
+			newPeers.push_back(neighbour);
+
+			neighbour.id = 11;
+			neighbour.staleness = 0;
+			neighbour.neighbourCount = 2;
+			newPeers.push_back(neighbour);
+			break;
+
+	case 15:
+			neighbour.id = 12;
+			neighbour.staleness = 0;
+			neighbour.neighbourCount = 3;
+			newPeers.push_back(neighbour);
+
+			neighbour.id = 16;
+			neighbour.staleness = 0;
+			neighbour.neighbourCount = 3;
+			newPeers.push_back(neighbour);
+			break;
+
+	case 11:
+			neighbour.id = 14;
+			neighbour.staleness = 0;
+			neighbour.neighbourCount = 3;
+			newPeers.push_back(neighbour);
+
+			neighbour.id = 10;
+			neighbour.staleness = 0;
+			neighbour.neighbourCount = 3;
+			newPeers.push_back(neighbour);
+			break;
+
+	case 17:
+			neighbour.id = 16;
+			neighbour.staleness = 0;
+			neighbour.neighbourCount = 3;
+			newPeers.push_back(neighbour);
+
+			neighbour.id = 14;
+			neighbour.staleness = 0;
+			neighbour.neighbourCount = 3;
+			newPeers.push_back(neighbour);
+			break;
+	}
 }
--- Gossip.h
+++ Gossip.h
@@ -3,29 +3,30 @@
 
 #include "VirtualApplication.h"
 #include <vector>
+#include <cmath>
 #include "GossipPacket_m.h"
 
 using namespace std;
 
+#define EPSILON 0.0001
+
 enum GOSSIP_CONTROL_MSGS {
-  PULL_NEIGHBOUR = 1,
-  PUSH_NEIGHBOUR = 2,
-  GOSSIP = 3,
-  GOSSIP_PUSH = 4,
-  GOSSIP_PULL = 5,
-  GOSSIP_BUSY = 6,
-  GOSSIP_ACK = 7
+	PULL_NEIGHBOUR = 1,
+	PUSH_NEIGHBOUR = 2,
+	GOSSIP = 3,
+	GOSSIP_PUSH = 4,
+	GOSSIP_PULL = 5,
+	GOSSIP_BUSY = 6,
+	GOSSIP_ACK = 7
 };
 
 enum GOSSIP_TIMERS {
-  GET_NEIGHBOUR = 1,
-  START_GOSSIP = 2,
-  SAMPLE_AVG = 3
+	GET_NEIGHBOUR = 1, START_GOSSIP = 2, SAMPLE_AVG = 3
 };
 
 struct peerInfo {
-  int id, neighbourCount, staleness;
-  double weight;
+	int id, neighbourCount, staleness;
+	double weight;
 };
 
 struct gossipExchMsg {
@@ -38,35 +39,37 @@ typedef struct peerInfo PEERINFO;
 typedef struct gossipExchMsg GOSSIP_EXCH_MSG;
 
 class Gossip: public VirtualApplication {
- private:
-  int packetsSent;
-  simtime_t neighbourCheckInterval, gossipInterval;
-  //To do: Implement using a list, it's a better option.
-  vector<PEERINFO> peers;
-  vector<PEERINFO> newPeers;
-  queue<GOSSIP_EXCH_MSG> waitQueue;
-  double gossipMsg;
-  bool isBusy;
-  short roundsBeforeStopping;
-  int expectedSeq;
-  short wait;
-  int rounds, lateResponse, droppedRequests;
-  int gSend, gReceive, gRespond;
+private:
+	int packetsSent;
+	simtime_t neighbourCheckInterval, gossipInterval;
+	//To do: Implement using a list, it's a better option.
+	vector<PEERINFO> peers;
+	vector<PEERINFO> newPeers;
+	queue<GOSSIP_EXCH_MSG> waitQueue;
+	double gossipMsg;
+	bool isBusy;
+	short roundsBeforeStopping;
+	int expectedSeq;
+	short wait;
+	int rounds, lateResponse, droppedRequests;
+	int gSend, gReceive, gRespond;
 
- protected:
-  void startup();
-  void finishSpecific();
-  void timerFiredCallback(int);
-  void handleSensorReading(SensorReadingMessage *);
-  void handleNeworkControlMessage(cMessage *);
-  void fromNetworkLayer(ApplicationPacket *, const char *, double, double);
+	bool compareDouble(double num1, double num2);
+	void assignNeighbours (int id);
 
-  void doGossip();
-  int getPeer();
-  GossipPacket* createGossipDataPacket(double , unsigned int );
-  GossipPacket* createGossipDataPacket(double , GossipInfo , unsigned int );
-  GossipPacket* createGossipDataPacket(double , int, unsigned int );
-  void enQueue(GOSSIP_EXCH_MSG);
-  void deQueue();
+protected:
+	void startup();
+	void finishSpecific();
+	void timerFiredCallback(int);
+	void handleSensorReading(SensorReadingMessage *);
+	void handleNeworkControlMessage(cMessage *);
+	void fromNetworkLayer(ApplicationPacket *, const char *, double, double);
+
+	int getPeer();
+	GossipPacket* createGossipDataPacket(double, unsigned int);
+	GossipPacket* createGossipDataPacket(double, GossipInfo, unsigned int);
+	GossipPacket* createGossipDataPacket(double, int, unsigned int);
+	void enQueue(GOSSIP_EXCH_MSG);
+	void deQueue();
 };
 #endif
